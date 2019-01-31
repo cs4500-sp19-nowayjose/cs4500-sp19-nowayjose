58a7eebc5c60b16b5e2dfc1e6323621edb292aa3
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-01-31 11:28:13 -0500

    Added columns to service model

--- src/main/java/edu/neu/cs4500/models/Service.java
+++ src/main/java/edu/neu/cs4500/models/Service.java
@@ -1,5 +1,16 @@
 package edu.neu.cs4500.models;
 
-public class Service {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="services")
+public class Service {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String serviceName;
 }
