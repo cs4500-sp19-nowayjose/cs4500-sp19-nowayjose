867e57c9924db37234d3032608523948101ca391
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-01-31 11:28:20 -0500

    Added getters and setters

--- src/main/java/edu/neu/cs4500/models/Service.java
+++ src/main/java/edu/neu/cs4500/models/Service.java
@@ -13,4 +13,16 @@ public class Service {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String serviceName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
 }
