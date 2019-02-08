df4632e69cc4c311b5f28a262159a4c9ae9613d4
naegahyung <naegahyung@gmail.com>
2019-02-07 19:32:49 -0500

    Added setters and getters for answer model

--- src/main/java/edu/neu/cs4500/models/Answer.java
+++ src/main/java/edu/neu/cs4500/models/Answer.java
@@ -13,6 +13,48 @@ public class Answer {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public User getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(User updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
