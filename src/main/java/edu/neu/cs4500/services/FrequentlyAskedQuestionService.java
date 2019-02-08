52cd7864e895f45749c6be60b8f849005f8a5eaa
Kathan Master <kathan.master@yahoo.com>
2019-02-08 16:56:27 -0500

    implemented endpoint for getting all faqs

--- src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
+++ src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
@@ -3,9 +3,12 @@ package edu.neu.cs4500.services;
 import edu.neu.cs4500.models.FrequentlyAskedQuestion;
 import edu.neu.cs4500.repositories.FrequentlyAskedQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FrequentlyAskedQuestionService {
@@ -15,5 +18,10 @@ public class FrequentlyAskedQuestionService {
     @PostMapping("/api/faq")
     public FrequentlyAskedQuestion createFaq(@RequestBody FrequentlyAskedQuestion faq) {
         return questionRepository.save(faq);
+    }
+
+    @GetMapping
+    public List<FrequentlyAskedQuestion> findAllFaqs() {
+        return (List<FrequentlyAskedQuestion>) questionRepository.findAll();
     }
 }
