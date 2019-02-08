b511443dbcecb7a97e35692577a962b811f4e3b7
Kathan Master <kathan.master@yahoo.com>
2019-02-08 16:44:04 -0500

    Create endpoint for adding a faq

--- src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
+++ src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
@@ -1,7 +1,10 @@
 package edu.neu.cs4500.services;
 
+import edu.neu.cs4500.models.FrequentlyAskedQuestion;
 import edu.neu.cs4500.repositories.FrequentlyAskedQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,8 @@ public class FrequentlyAskedQuestionService {
     @Autowired
     FrequentlyAskedQuestionRepository questionRepository;
 
-    
+    @PostMapping("/api/faq")
+    public FrequentlyAskedQuestion createFaq(@RequestBody FrequentlyAskedQuestion faq) {
+        return questionRepository.save(faq);
+    }
 }
