e8f0ce33fee50e266b55a557296df3302b892f4a
Kathan Master <kathan.master@yahoo.com>
2019-02-08 17:46:29 -0500

    Implemented endpoint for getting faq by id

--- src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
+++ src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
@@ -3,12 +3,10 @@ package edu.neu.cs4500.services;
 import edu.neu.cs4500.models.FrequentlyAskedQuestion;
 import edu.neu.cs4500.repositories.FrequentlyAskedQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FrequentlyAskedQuestionService {
@@ -20,8 +18,15 @@ public class FrequentlyAskedQuestionService {
         return questionRepository.save(faq);
     }
 
-    @GetMapping
+    @GetMapping("/api/faq")
     public List<FrequentlyAskedQuestion> findAllFaqs() {
         return (List<FrequentlyAskedQuestion>) questionRepository.findAll();
+    }
+
+    @GetMapping("/api/faq/{faqId}")
+    public FrequentlyAskedQuestion findFaqById(
+            @PathVariable("faqId") Integer id) {
+        Optional<FrequentlyAskedQuestion> optionalQuestion = questionRepository.findById(id);
+        return optionalQuestion.orElse(null);
     }
 }
