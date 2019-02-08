2f68141a392b1ad2283492fe31d90e4f8de3a822
Kathan Master <kathan.master@yahoo.com>
2019-02-08 16:38:51 -0500

    Added autowired question repository to faq sservice

--- src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
+++ src/main/java/edu/neu/cs4500/services/FrequentlyAskedQuestionService.java
@@ -1,4 +1,13 @@
 package edu.neu.cs4500.services;
 
+import edu.neu.cs4500.repositories.FrequentlyAskedQuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FrequentlyAskedQuestionService {
+    @Autowired
+    FrequentlyAskedQuestionRepository questionRepository;
+
+    
 }
