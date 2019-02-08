b4235cca3085d5dfe85175a32b17d386fb77a9e6
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-02-08 15:49:58 -0500

    Added FAQ repository

---
+++ src/main/java/edu/neu/cs4500/repositories/FrequentlyAskedAnswerRepository.java
@@ -0,0 +1,9 @@
+package edu.neu.cs4500.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.neu.cs4500.models.FrequentlyAskedAnswer;
+
+public interface FrequentlyAskedAnswerRepository  extends CrudRepository<FrequentlyAskedAnswer, Integer>{
+
+}
