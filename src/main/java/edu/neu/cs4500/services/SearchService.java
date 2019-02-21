2861587fae90a759c3280e772916c00282201c6e
naegahyung <naegahyung@gmail.com>
2019-02-21 18:25:16 -0500

    Added junit for testing and created boiler test file for search flow

---
+++ src/main/java/edu/neu/cs4500/services/SearchService.java
@@ -0,0 +1,18 @@
+package edu.neu.cs4500.services;
+
+import edu.neu.cs4500.models.Service;
+import edu.neu.cs4500.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchService {
+
+    public SearchService() {
+
+    }
+
+    public List<User> searchForProviders(Service service, String criteria) {
+        return new ArrayList<User>();
+    }
+}
