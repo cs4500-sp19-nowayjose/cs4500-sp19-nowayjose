6c81d0f85712c307fff4ca78bbf26cd165c6b76f
Benjamin Clauss <benjamin.h.clauss@gmail.com>
2019-03-15 17:01:17 -0400

    Add cross origin to service service

--- src/main/java/edu/neu/cs4500/services/ServiceService.java
+++ src/main/java/edu/neu/cs4500/services/ServiceService.java
@@ -1,23 +1,17 @@
 package edu.neu.cs4500.services;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.neu.cs4500.models.Service;
 import edu.neu.cs4500.models.ServiceCategory;
 import edu.neu.cs4500.repositories.ServiceCategoryRepository;
 import edu.neu.cs4500.repositories.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins="*")
 public class ServiceService {
 	@Autowired
 	ServiceRepository serviceRepository;
