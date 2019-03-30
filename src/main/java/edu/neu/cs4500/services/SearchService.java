74fac3a6894827e3895026489c4c23181ac75270
Calvin Zhao <MmZzyh@Calvins-MacBook.local>
2019-03-30 17:12:03 -0400

    Search controller that returns all users

--- src/main/java/edu/neu/cs4500/services/SearchService.java
+++ src/main/java/edu/neu/cs4500/services/SearchService.java
@@ -3,17 +3,33 @@ package edu.neu.cs4500.services;
 import edu.neu.cs4500.models.SearchCriteria;
 import edu.neu.cs4500.models.Service;
 import edu.neu.cs4500.models.User;
+import edu.neu.cs4500.repositories.ServiceQuestionRepository;
+import edu.neu.cs4500.repositories.ServiceRepository;
 import edu.neu.cs4500.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
+@RestController
+@CrossOrigin(origins="*")
 public class SearchService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ServiceRepository serviceRepository;
+    @Autowired
+    private ServiceQuestionRepository serviceQuestionRepository;
 
-    public List<User> searchForProviders(Service service, SearchCriteria criteria) {
+    @GetMapping("/api/provider-search/{serviceId}")
+    public List<User> searchProvidersForService(@PathVariable("serviceId") Integer serviceId)
+    {
+        return userRepository.findAllUsers();
+    }
+
+    List<User> searchForProviders(Service service, SearchCriteria criteria) {
         return criteria.orderAndFilterUsersByScore(userRepository.findAllUsers());
     }
 
