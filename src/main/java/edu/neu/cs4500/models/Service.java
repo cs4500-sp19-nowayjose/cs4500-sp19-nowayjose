6045af16f2f29cd0d850abfb0829693675fd2af6
Kathan Master <kathan.master@yahoo.com>
2019-04-01 22:47:12 -0400

    refactored to handle null case

--- src/main/java/edu/neu/cs4500/models/Service.java
+++ src/main/java/edu/neu/cs4500/models/Service.java
@@ -19,7 +19,7 @@ public class Service {
 	private List<ServiceCategory> serviceCategories;
 
 	public String getImageUrl() {
-		return this.imageUrl;
+		return this.imageUrl != null ? this.imageUrl : "https://yt3.ggpht.com/a-/AAuE7mDk-PBd6ZWOvXZ6VFQZ5b1SgLqlCq1aPU8tBw=s288-mo-c-c0xffffffff-rj-k-no";
 	}
 
 	public void setImageUrl(String imageUrl) {
